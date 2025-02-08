2dafe0ca929d552bb081b75fa075f9ecc7b55d8d
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-08 11:46:19 +0000

    Auto Commit

--- ShelfTrack/backend/core/models.py
+++ ShelfTrack/backend/core/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Student(models.Model):
     roll_no = models.CharField(max_length=2)
     name = models.CharField(max_length=100)
-    div = models.CharField(max_length=2)
-    
+    grade = models.CharField(max_length=10)
+    division = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.roll_no} - {self.name} (Class {self.grade} {self.division})"
