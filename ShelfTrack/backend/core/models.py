aa8959c455b6b3e51715af36c78fa343e16e660b
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-08 11:44:20 +0000

    Auto Commit

--- ShelfTrack/backend/core/models.py
+++ ShelfTrack/backend/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    roll_no = models.CharField(max_length=2)
+    name = models.CharField(max_length=100)
+    div = models.CharField(max_length=2)
+    
