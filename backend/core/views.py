b68d822f61ff6b9f226a6a9bc9202c3b10866674
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-13 09:29:27 +0000

    Auto Commit

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from .models import Student
+from django.http import HttpResponse
 
 def student_list(request):
     students = list(Student.objects.values())
     return JsonResponse({"students": students})
+
+def home(request):
+    return HttpResponse("<h1>Welcome to ShelfTrack - Library Management System</h1>")
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home  # Import the home view
 
 urlpatterns = [
+    path('', home, name='home'),  # Home page
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
