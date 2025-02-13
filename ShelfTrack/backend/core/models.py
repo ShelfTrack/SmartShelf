327f59fe42112709407dbb2a5cb2d04777bf388b
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-13 08:46:51 +0000

    Auto Commit

--- ShelfTrack/backend/core/models.py
+++ ShelfTrack/backend/core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 class Student(models.Model):
-    roll_no = models.CharField(max_length=2)
-    name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=10, unique=True)
+    roll_no = models.PositiveIntegerField()
+    name = models.CharField(max_length=200)
     grade = models.CharField(max_length=10)
     division = models.CharField(max_length=5)
 
-    def __str__(self):
+    class Meta:
+        unique_together = ("roll_no","grade","divsion")
+    
+    def _str_(self):
         return f"{self.roll_no} - {self.name} (Class {self.grade} {self.division})"
