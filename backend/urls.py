d913ec848d6a204923aba7d72a675833796b715f
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-13 09:54:33 +0000

    Auto Commit

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home  # Import the home view
+from core.views import home
 
 urlpatterns = [
-    path('', home, name='home'),  # Home page
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
